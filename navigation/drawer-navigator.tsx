8f825ae41c1f5c592010d3bb823a4a10f42fe3c2
Vishnu Chandra MC <vishnuchandra.mc@gmail.com>
2024-04-07 17:53:07 +0530

    Initial commit
    Generated by create-expo-stack 2.0.0.

---
+++ navigation/drawer-navigator.tsx
@@ -0,0 +1,38 @@
+import { Ionicons, MaterialIcons } from '@expo/vector-icons';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { StackScreenProps } from '@react-navigation/stack';
+import { HeaderButton } from 'components/HeaderButton';
+
+import { RootStackParamList } from '.';
+import TabNavigator from './tab-navigator';
+import Home from '../screens/home';
+
+type Props = StackScreenProps<RootStackParamList, 'DrawerNavigator'>;
+
+const Drawer = createDrawerNavigator();
+
+export default function DrawerNavigator({ navigation }: Props) {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen
+        name="Home"
+        component={Home}
+        options={{
+          drawerIcon: ({ size, color }) => (
+            <Ionicons name="home-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Tabs"
+        component={TabNavigator}
+        options={{
+          headerRight: () => <HeaderButton onPress={() => navigation.navigate('Modal')} />,
+          drawerIcon: ({ size, color }) => (
+            <MaterialIcons name="border-bottom" size={size} color={color} />
+          ),
+        }}
+      />
+    </Drawer.Navigator>
+  );
+}
---
+++ screens/home.tsx
@@ -0,0 +1,5 @@
+import { ScreenContent } from 'components/ScreenContent';
+
+export default function Home() {
+  return <ScreenContent path="screens/home.tsx" title="Home" />;
+}
